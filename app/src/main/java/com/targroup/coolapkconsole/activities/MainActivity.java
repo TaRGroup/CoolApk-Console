25856ee11e5792fdcbf17c9bbdf41e2741d322fb
Rachel030219 <291054446@qq.com>
2017-01-30 11:39:56 +0800

    除了导入 jsoup 之外啥都没干……

---
+++ app/src/main/java/com/targroup/coolapkconsole/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.targroup.coolapkconsole.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.targroup.coolapkconsole.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
