41d6beeda02ab82dfb71b8cc7aa6d32f79e08dea
liangyuteng0927 <liangyuteng0927@hotmail.com>
2017-01-31 10:15:44 +0800

    Add simple listview , item arraylist and adapter.

--- app/src/main/java/com/targroup/coolapkconsole/activities/MainActivity.java
+++ app/src/main/java/com/targroup/coolapkconsole/activities/MainActivity.java
@@ -6,8 +6,14 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import android.support.design.widget.NavigationView;
@@ -21,10 +27,14 @@ import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import com.targroup.coolapkconsole.R;
+import com.targroup.coolapkconsole.model.AppItem;
 import com.targroup.coolapkconsole.utils.JsoupUtil;
 import com.targroup.coolapkconsole.view.BezelImageView;
 
 import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar mToolbar;
@@ -40,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
     private String mUserName;
     private String mAvatarUrl;
     private Bitmap mAvatar;
+
+    private ListView mListView;
+    private AppListAdapter mAdapter;
+    private List<AppItem> mAppsList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,6 +89,15 @@ public class MainActivity extends AppCompatActivity {
         mImageViewUserAvatar = (BezelImageView)header.findViewById(R.id.material_drawer_account_header_current);
         mTextViewUserName = (TextView)header.findViewById(R.id.material_drawer_account_header_name);
         mTextViewUserEmail = (TextView)header.findViewById(R.id.material_drawer_account_header_email);
+
+        mListView = (ListView)findViewById(R.id.list);
+        // TODO:例子加载数据
+        mAppsList.add(new AppItem(null, "Name-1", "Pkg", "1.0", "1", "1", "Type", "Tag", "Author",
+                "Downloads", "Creator", "Updater", "LastUpdate", "Status"));
+        mAppsList.add(new AppItem(null, "Name-2", "Pkg", "1.0", "1", "1", "Type", "Tag", "Author",
+                "Downloads", "Creator", "Updater", "LastUpdate", "Status"));
+        mAdapter = new AppListAdapter();
+        mListView.setAdapter(mAdapter);
 
         // Init Data Now!
         mLoadInfoTask = new LoadInfoTask();
@@ -127,5 +150,27 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+    private class AppListAdapter extends ArrayAdapter<AppItem> {
+        AppListAdapter () {
+            super(MainActivity.this, 0, mAppsList);
+        }
+        @Override
+        public @NonNull View getView(int position, @Nullable View convertView,
+                                     @NonNull ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.item_app, null);
+            }
+            AppItem item = mAppsList.get(position);
+            ImageView icon = (ImageView)convertView.findViewById(R.id.item_icon);
+            TextView title = (TextView)convertView.findViewById(R.id.item_title);
+            TextView subtitle = (TextView)convertView.findViewById(R.id.item_subtitle);
+            TextView context = (TextView)convertView.findViewById(R.id.item_context);
+            icon.setImageBitmap(item.getIcon());
+            title.setText(item.getName());
+            subtitle.setText(item.getStatus());
+            context.setText(item.getAuthor());
+            return convertView;
+        }
     }
 }
