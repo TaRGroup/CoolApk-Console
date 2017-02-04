8b683d7853d8b70f07b1541cdab40607221bf5fa
Rachel030219 <291054446@qq.com>
2017-02-04 10:14:39 +0800

    移除无用资源文件，添加了 Palette 动态取色+变色，准备下次移除。（雾）大概搞了搞 DetailActivity ，应该有点什么东西能看了…

--- app/src/main/java/com/targroup/coolapkconsole/activities/DetailActivity.java
+++ app/src/main/java/com/targroup/coolapkconsole/activities/DetailActivity.java
@@ -1,11 +1,22 @@
 package com.targroup.coolapkconsole.activities;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import com.targroup.coolapkconsole.R;
 import com.targroup.coolapkconsole.model.AppItem;
@@ -14,6 +25,12 @@ import com.targroup.coolapkconsole.utils.Util;
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_APP_ITEM = DetailActivity.class.getSimpleName() + "/EXTRA_APP_ITEM";
     private AppItem mAppItem;
+
+    private ImageView mIcon;
+    private TextView mName;
+    private TextView mStatus;
+    private TextView mPackage;
+    private Bitmap icon = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +45,48 @@ public class DetailActivity extends AppCompatActivity {
             finish();
             return;
         }
+
+        bindViews();
+    }
+    public void bindViews(){
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        CollapsingToolbarLayout toolbarLayout = (CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.detail_fab);
+
+        mIcon = (ImageView)findViewById(R.id.detail_icon);
+        mName = (TextView) findViewById(R.id.detail_name);
+        mStatus = (TextView)findViewById(R.id.detail_status);
+        mPackage = (TextView)findViewById(R.id.detail_packageName);
+
+        icon = ImageLoader.getInstance().loadImageSync(mAppItem.getIcon(),new DisplayImageOptions.Builder().cacheOnDisk(true).build());
+        if (icon != null) {
+            // TODO:下一步这里应该做成类似 CoolApk 客户端那样， Collapsing 使用高斯模糊处理后的图标
+            toolbarLayout.setBackgroundColor(Palette.from(icon).generate().getVibrantSwatch().getRgb());
+            fab.setBackgroundTintList(ColorStateList.valueOf(Palette.from(icon).generate().getVibrantSwatch().getRgb()));
+            mIcon.setImageBitmap(icon);
+        }
+        mName.setText(mAppItem.getName());
+        mStatus.setText(mAppItem.getStatus());
+        mPackage.setText(mAppItem.getPackageName());
+
         setSupportActionBar(toolbar);
         setTitle(mAppItem.getName());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         if (Util.getPublishState() != Util.PublishState.PUBLISH_STATE_STABLE) {
             Snackbar.make(toolbar, "ID:" + mAppItem.getId(), Snackbar.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
