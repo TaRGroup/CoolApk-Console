b4e6aa316922d3fe1e14b857e33b85f300ba28d1
Trumeet <liangyuteng12345@gmail.com>
2017-04-02 17:58:04 +0800

    除了MainActivity以外换用RxJava

--- app/src/main/java/com/targroup/coolapkconsole/activities/DetailActivity.java
+++ app/src/main/java/com/targroup/coolapkconsole/activities/DetailActivity.java
@@ -1,13 +1,8 @@
 package com.targroup.coolapkconsole.activities;
 
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.net.Uri;
-import android.support.v4.widget.NestedScrollView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,34 +14,24 @@ import android.widget.TextView;
 import android.graphics.Bitmap;
 
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.CollapsingToolbarLayout;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.GlideBitmapDrawable;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.targroup.coolapkconsole.R;
 import com.targroup.coolapkconsole.model.AppDetail;
 import com.targroup.coolapkconsole.model.AppItem;
 import com.targroup.coolapkconsole.model.DownloadStatItem;
-import com.targroup.coolapkconsole.utils.JsoupUtil;
+import com.targroup.coolapkconsole.utils.CoolapkApi;
 import com.targroup.coolapkconsole.utils.Util;
-
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import im.dacer.androidcharts.LineView;
+import rx.Subscriber;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_APP_ITEM = DetailActivity.class.getSimpleName() + "/EXTRA_APP_ITEM";
@@ -108,6 +93,14 @@ public class DetailActivity extends AppCompatActivity {
         bindViews();
         refreshDetail();
     }
+
+    @Override
+    public void onDestroy () {
+        if (!mLoadDetailSubscriber.isUnsubscribed())
+            mLoadDetailSubscriber.unsubscribe();
+        super.onDestroy();
+    }
+
     public void bindViews(){
         ButterKnife.bind(this);
         mDownloadsChart.setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY); //optional
@@ -127,10 +120,73 @@ public class DetailActivity extends AppCompatActivity {
             Snackbar.make(mToolbar, "ID:" + mAppItem.getId(), Snackbar.LENGTH_SHORT).show();
         }
     }
-    private LoadDetailTask mLoadDetailTask;
+    private Subscriber<Object> mLoadDetailSubscriber = new Subscriber<Object>() {
+        @Override
+        public void onCompleted() {
+            mProgress.setVisibility(View.GONE);
+            mContentView.setEnabled(true);
+            //
+            mVersion.setText(String.format(res.getString(R.string.detail_version),mAppItem.getVersion()));
+            mSize.setText(String.format(res.getString(R.string.detail_size),mAppItem.getSize()));
+            mLastUpdate.setText(String.format(res.getString(R.string.detail_last),mAppItem.getLastUpdate()));
+            mDownloads.setText(String.format(res.getString(R.string.detail_downloads),mAppItem.getDownloads()));
+            mUpdater.setText(String.format(res.getString(R.string.detail_updater),mAppItem.getUpdater()));
+
+            ArrayList<ArrayList<Integer>> series = new ArrayList<>();
+            ArrayList<Integer> downloadsList = new ArrayList<>();
+            ArrayList<Integer> installList = new ArrayList<>();
+            ArrayList<String> bottom = new ArrayList<>();
+            int count = 0;
+            // 0xFF56B7F1
+            ArrayList<DownloadStatItem> downloadStatItems = mDetail.getmStats();
+            Collections.reverse(downloadStatItems);
+            for (DownloadStatItem item : downloadStatItems) {
+                downloadsList.add(Integer.parseInt(item.getmDownloads()));
+                installList.add(Integer.parseInt(item.getmInstalls()));
+                bottom.add(item.getmDate());
+                count++;
+                if (count >= 6) break; // Do not show too many data in preview chart.
+            }
+            series.add(downloadsList);
+            series.add(installList);
+            mDownloadsChart.setBottomTextList(bottom);
+            mDownloadsChart.setDataList(series);
+            mDownloadsChart.setColorArray(new int[]{mColor,
+                    Color.parseColor("#FFC93437")});
+
+            mIcon.setImageBitmap(icon);
+            mToolBarLayout.setBackgroundColor(mColor);
+            mToolBarLayout.setContentScrimColor(mColor);
+            mToolBarLayout.setStatusBarScrimColor(mColor);
+            mToolbar.setBackgroundColor(mColor);
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            mProgress.setVisibility(View.GONE);
+            mContentView.setEnabled(true);
+        }
+
+        @Override
+        public void onNext(Object o) {
+            if (o instanceof AppDetail) {
+                mDetail = (AppDetail)o;
+            } else if (o instanceof Integer) {
+                mColor = (Integer)o;
+            } else if (o instanceof Bitmap) {
+                icon = (Bitmap)o;
+            }
+        }
+
+        @Override
+        public void onStart () {
+            mProgress.setVisibility(View.VISIBLE);
+            mContentView.setEnabled(false);
+        }
+    };
+
     private void refreshDetail () {
-        mLoadDetailTask = new LoadDetailTask();
-        mLoadDetailTask.execute();
+        CoolapkApi.getAppDetail(this, mAppItem).subscribe(mLoadDetailSubscriber);
     }
 
     @Override
@@ -160,120 +216,6 @@ public class DetailActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private class LoadDetailTask extends AsyncTask<Void, Void, Object> {
-        @Override
-        protected Object doInBackground(Void... params) {
-            try {
-                Document detailDoc = JsoupUtil.getDocument("developer.coolapk.com/do?c=apk&m=edit&id="+mAppItem.getId()
-                        , true);
-                String type = detailDoc.select("select[name=softtype]").select("[selected]").text();
-                String catid = detailDoc.select("select[name=catid]").select("[selected]").text();
-                String keywords = detailDoc.select("input[class=mdl-textfield__input][name=keywords]").attr("value");
-                Elements detailElements = detailDoc.select("script[id=ue-apk-editor]");
-                String detail = detailElements.select("p").html();
-                String language = detailDoc.select("select[name=language]").select("[selected]").text();
-                Elements imageDiv = detailDoc.select("div[id=apkImageUploaderList]");
-                ArrayList<String> imageUrls = new ArrayList<>();
-                for (Element element : imageDiv) {
-                    imageUrls.add(element.select("img").attr("src"));
-                }
-                Elements downloads = detailDoc.select("div[id^=apk_edit__tab-panel-3]");
-                Elements downloadsTr = downloads.select("tbody").get(0).select("tr");
-                mDetail = new AppDetail();
-                for (Element element : downloadsTr) {
-                    Elements td = element.select("td");
-                    DownloadStatItem item = new DownloadStatItem();
-                    item.setmDate(td.get(0).text());
-                    item.setmAppName(td.get(1).text());
-                    item.setmVersion(td.get(2).text());
-                    item.setmDownloads(td.get(3).text());
-                    item.setmDownloadsStation(td.get(3).text());
-                    item.setmDownloadsOutsideStation(td.get(4).text());
-                    item.setmDownloadsNew(td.get(5).text());
-                    item.setmInstalls(td.get(6).text());
-                    item.setmInstallsNew(td.get(7).text());
-                    item.setmSize(td.get(8).text());
-                    mDetail.getmStats().add(item);
-                }
-                Collections.sort(mDetail.getmStats(), new Comparator<DownloadStatItem>() {
-                    @Override
-                    public int compare(DownloadStatItem downloadStatItem
-                            , DownloadStatItem t1) {
-                        int date1 = Integer.parseInt(downloadStatItem.getmDate());
-                        int date2 = Integer.parseInt(t1.getmDate());
-                        if (date1 > date2)
-                            return 1;
-                        if (date1 < date2)
-                            return -1;
-                        return 0;
-                    }
-                });
-                mDetail.setType(type);
-                mDetail.setCatId(catid);
-                mDetail.setKeyWords(keywords.split(","));
-                mDetail.setDetail(detail);
-                mDetail.setImageUrls(imageUrls);
-                mDetail.setLanguage(language);
-
-                icon = Glide.with(DetailActivity.this)
-                        .load(mAppItem.getIcon())
-                        .asBitmap()
-                        .into(125, 125).get();
-                mColor = Palette.from(icon)
-                        .generate().getVibrantColor(getResources().getColor(R.color.colorPrimary));
-                return null;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return e;
-            }
-        }
-
-        @Override
-        protected void onPreExecute () {
-            mProgress.setVisibility(View.VISIBLE);
-            mContentView.setEnabled(false);
-        }
-        @Override
-        protected void onPostExecute (Object o) {
-            mProgress.setVisibility(View.GONE);
-            mContentView.setEnabled(true);
-
-            mVersion.setText(String.format(res.getString(R.string.detail_version),mAppItem.getVersion()));
-            mSize.setText(String.format(res.getString(R.string.detail_size),mAppItem.getSize()));
-            mLastUpdate.setText(String.format(res.getString(R.string.detail_last),mAppItem.getLastUpdate()));
-            mDownloads.setText(String.format(res.getString(R.string.detail_downloads),mAppItem.getDownloads()));
-            mUpdater.setText(String.format(res.getString(R.string.detail_updater),mAppItem.getUpdater()));
-
-            ArrayList<ArrayList<Integer>> series = new ArrayList<>();
-            ArrayList<Integer> downloadsList = new ArrayList<>();
-            ArrayList<Integer> installList = new ArrayList<>();
-            ArrayList<String> bottom = new ArrayList<>();
-            int count = 0;
-            // 0xFF56B7F1
-            ArrayList<DownloadStatItem> downloadStatItems = mDetail.getmStats();
-            Collections.reverse(downloadStatItems);
-            for (DownloadStatItem item : downloadStatItems) {
-                downloadsList.add(Integer.parseInt(item.getmDownloads()));
-                installList.add(Integer.parseInt(item.getmInstalls()));
-                bottom.add(item.getmDate());
-                count++;
-                if (count >= 6) break; // Do not show too many data in preview chart.
-            }
-            series.add(downloadsList);
-            series.add(installList);
-            mDownloadsChart.setBottomTextList(bottom);
-            mDownloadsChart.setDataList(series);
-            mDownloadsChart.setColorArray(new int[]{mColor,
-            Color.parseColor("#FFC93437")});
-
-            mIcon.setImageBitmap(icon);
-            mToolBarLayout.setBackgroundColor(mColor);
-            mToolBarLayout.setContentScrimColor(mColor);
-            mToolBarLayout.setStatusBarScrimColor(mColor);
-            mToolbar.setBackgroundColor(mColor);
         }
     }
 }
